adaca3ba4830c19462b403f9f51eaa67f79b4078
Emmanuel Siamoonga <emmanuelsiamoonga@gmail.com>
2025-01-22 03:07:41 +0200

    COMMITTED SUCCESSFULLY

--- src/Arrays3.java
+++ src/Arrays3.java
@@ -6,10 +6,12 @@ public class Arrays3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any 5 Values:");
         int[] nums = new int[5];
+        int sum=0;
         for(int i=0; i<nums.length; i++){
             nums[i] = sc.nextInt();
-           // int sum += nums[i];
+            sum += nums[i];
         }
+        System.out.println("THE SUM OF THE NUMBERS YOU ENTERED IS "+sum);
         sc.close();
     }
     
--- src/sum.cpp
+++ src/sum.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int numbers[5];
     int sum=0;
-    cout<<"Enter any 5 numbers"<<endl;
+    cout<<"Enter 5 numbers"<<endl;
     for(int x=0; x<5; x++){
         cin>>numbers[x];
         sum += numbers[x];
